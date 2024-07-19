c21f71bcf8e3ffb57ab296d8154cd7a63c994636
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2024-07-19 12:32:17 +0000

    code update

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-let users = []; // Global variable to store user information
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'public', 'views')); // Updated path
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
     res.render('voter-registration');
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    users.push({ username, password });
-    res.redirect('/login');
-});
-
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    const user = users.find(u => u.username === username && u.password === password);
-
-    if (user) {
-        res.redirect('/dashboard');
-    } else {
-        res.send('Invalid login credentials');
-    }
-});
-
-app.get('/dashboard', (req, res) => {
-    res.render('dashboard');
 });
 
 app.listen(3000, () => {
